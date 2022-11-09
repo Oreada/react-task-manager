8850dac9531a9bd1d343c5ca44a271ea9cc4c5e8
Olga Antushevich <antushevich@gmail.com>
2022-11-09 20:02:17 +0200

    feat: add constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,2 @@
-export const BASE_API = 'https://rss-react-project.herokuapp.com';
+export const URL_BASE = 'https://rss-react-project.herokuapp.com';
+export const URL_SINGUP = `${URL_BASE}/auth/signup`;
