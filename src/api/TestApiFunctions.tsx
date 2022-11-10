8421fcb006ba355f53abfb3d80182354884b5876
Olga Antushevich <antushevich@gmail.com>
2022-11-10 11:41:29 +0200

    feat: implement function getUser

--- src/api/TestApiFunctions.tsx
+++ src/api/TestApiFunctions.tsx
@@ -1,34 +1,38 @@
 import { useState } from 'react';
 import { UserInfo } from 'types/types';
-import { getAllUsers } from './users/getAllUsers';
+import { getUser } from './users/getUser';
 
+// 636b5a6b83f1e2fe95e7a283
 export const firstUser = {
   name: 'FirstUser',
   login: 'FirstUser',
   password: 'FirstUser',
 };
 
+// 636b7dd719d35b6ca446c3cd
 export const secondUser = {
   name: 'SecondUser',
   login: 'SecondUser',
   password: 'SecondUser',
 };
 
+// 636ba2d019d35b6ca446c404
 export const thirdUser = {
   login: 'ThirdUser',
   password: 'ThirdUser',
 };
 
 export function TestApiFunctions() {
-  const [result, setResult] = useState<Array<UserInfo>>([]);
+  const [result, setResult] = useState<UserInfo | unknown>({});
   const [error, setError] = useState<string>('');
 
   const clickHandler = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
 
     try {
-      const result = await getAllUsers(
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmJhMmQwMTlkMzViNmNhNDQ2YzQwNCIsImxvZ2luIjoiVGhpcmRVc2VyIiwiaWF0IjoxNjY4MDY5NTk1LCJleHAiOjE2NjgxMTI3OTV9.rChnyK0_5zvXhQZyRBQGrjNAVHQiTjZJ3x4WtTwczX8'
+      const result = await getUser(
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmJhMmQwMTlkMzViNmNhNDQ2YzQwNCIsImxvZ2luIjoiVGhpcmRVc2VyIiwiaWF0IjoxNjY4MDY5NTk1LCJleHAiOjE2NjgxMTI3OTV9.rChnyK0_5zvXhQZyRBQGrjNAVHQiTjZJ3x4WtTwczX8',
+        '636b5a6b83f1e2fe95e7a283'
       );
       console.log('my result is', result);
       setResult(result);
---
+++ src/api/users/getUser.ts
@@ -0,0 +1,27 @@
+import { URL_GET_ALL_USERS } from 'constants/constants';
+import { UserInfo } from 'types/types';
+
+//! Find User
+export async function getUser(token: string, idUser: string): Promise<UserInfo> {
+  try {
+    const response = await fetch(`${URL_GET_ALL_USERS}/${idUser}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status code ${response.status}`);
+    }
+
+    const user = await response.json();
+    return user;
+  } catch (e: unknown) {
+    const err = e as Error;
+    console.log('Catched error =', err.message);
+    throw new Error(err.message);
+  }
+}
