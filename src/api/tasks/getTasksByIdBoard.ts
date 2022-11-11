5dac8001859be7c6ebf0b4babcabd8c39b53e959
Olga Antushevich <antushevich@gmail.com>
2022-11-11 12:45:58 +0200

    feat: implement function getTasksByIdBoard

---
+++ src/api/tasks/getTasksByIdBoard.ts
@@ -0,0 +1,27 @@
+import { URL_TASKS_SET } from 'constants/constants';
+import { Task } from 'types/types';
+
+//! Get Tasks in selected Board
+export async function getTasksByIdBoard(token: string, idBoard: string): Promise<Array<Task>> {
+  try {
+    const response = await fetch(`${URL_TASKS_SET}/${idBoard}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status code ${response.status}`);
+    }
+
+    const tasksList = await response.json();
+    return tasksList;
+  } catch (e: unknown) {
+    const err = e as Error;
+    console.log('Catched error =', err.message);
+    throw new Error(err.message);
+  }
+}
