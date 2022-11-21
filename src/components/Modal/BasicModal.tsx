26bf263b7623a184f2afbf8d2ca32f921283f044
Olga Antushevich <antushevich@gmail.com>
2022-11-21 18:37:36 +0200

    feat: change the BasicModal structure

--- src/components/Modal/BasicModal.tsx
+++ src/components/Modal/BasicModal.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { TaskType } from 'types/types';
 
 interface ModalProps {
   children: React.ReactNode;
   title: string;
-  func: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<TaskType | void> | void;
+  openModal: boolean;
+  setOpenModal: (arg: boolean) => void;
 }
 
 const style = {
@@ -28,7 +26,6 @@ const style = {
   gap: 5,
 
   bgcolor: 'background.paper',
-  // border: '2px solid #000',
   boxShadow: 24,
   p: 5,
 };
@@ -43,23 +40,11 @@ const theme = createTheme({
 });
 
 export function BasicModal(props: ModalProps) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
   return (
     <>
-      <Button
-        variant="outlined"
-        endIcon={<AddCircleRoundedIcon />}
-        color="success"
-        onClick={handleOpen}
-      >
-        {props.title}
-      </Button>
       <Modal
-        open={open}
-        onClose={handleClose}
+        open={props.openModal}
+        onClose={() => props.setOpenModal(false)}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
@@ -70,18 +55,6 @@ export function BasicModal(props: ModalProps) {
             </Typography>
 
             {props.children}
-
-            <Button
-              variant="outlined"
-              size="large"
-              color="success"
-              onClick={(event) => {
-                props.func(event);
-                handleClose();
-              }}
-            >
-              Add
-            </Button>
           </ThemeProvider>
         </Box>
       </Modal>
