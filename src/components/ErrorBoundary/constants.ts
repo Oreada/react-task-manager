8d521c03c77ced9732e7f6fd27c2108ff4cf1d7b
ps0m <ps0m@tut.by>
2022-11-25 13:58:26 +0300

    fix: correct grammatical errors

--- src/components/ErrorBoundary/constants.ts
+++ src/components/ErrorBoundary/constants.ts
@@ -1,2 +1,2 @@
-export const TITLE = 'Ooops.. something error';
-export const BUTTON_TEXT = 'To Home';
+export const TITLE = `Ooops.. something's wrong`;
+export const BUTTON_TEXT = 'To the home page';
